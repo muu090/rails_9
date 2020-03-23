ade46bce34095a49eebf7a4dfab3d5a446bda50a
Sota <sooota09@gmail.com>
2020-03-23 15:59:52 +0900

    Iniri

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '1319f6b4077f1032bb0c70b7acc312e7a2ffd16087806e2fe473f984b9f76913de27251eb0204148412829813d8d4145cff3539f6782fb869f81651718e361f0'
